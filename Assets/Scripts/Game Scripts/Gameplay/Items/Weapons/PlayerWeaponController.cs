31976350aa5397e5509509b78ca6c8e024852064
Denny Scott <dennyscott301@gmail.com>
2015-03-24 18:56:04 -0500

    First Update Level

---
+++ Assets/Scripts/Game Scripts/Gameplay/Items/Weapons/PlayerWeaponController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeaponController : MonoBehaviour {
+
+	public GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+ 			
+	}
+
+	public void EquipWeapon() {
+		weapon.SetActive(true);
+	}
+}
