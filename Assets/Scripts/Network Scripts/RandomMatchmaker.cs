78c1749eeb7c4b1f3d0c484e48272e3f447046f7
Travis Scott <travisscott301@gmail.com>
2015-03-24 21:23:54 -0500

    Added network capabilities

---
+++ Assets/Scripts/Network Scripts/RandomMatchmaker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+ 
+public class RandomMatchmaker : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+ 
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby() {
+    	PhotonNetwork.JoinRandomRoom();
+	}
+
+    void OnPhotonRandomJoinFailed() {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom() {
+    	GameObject player = PhotonNetwork.Instantiate("FirstPerson", Vector3.zero, Quaternion.identity, 0);
+    	player.GetComponent<CharacterController>().enabled = true;
+    	player.GetComponent<FirstPersonController>().enabled = true;
+    	player.GetComponent<AudioSource>().enabled = true;
+    	player.GetComponentInChildren<Camera>().enabled = true;
+    	player.GetComponentInChildren<AudioListener>().enabled = true;
+    }
+}
