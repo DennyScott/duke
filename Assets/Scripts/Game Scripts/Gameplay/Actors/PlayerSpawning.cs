2c17dddb7b67b3fb3d39256693411e33d2527d85
Travis Scott <travisscott301@gmail.com>
2015-04-14 14:51:34 -0500

    Spawn Points Added

---
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/PlayerSpawning.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawning : MonoBehaviour {
+
+    public System.Action OnSpawnAction;
+    private Transform _transform ;
+    private PlayerHealth _health;
+
+	// Use this for initialization
+	void Start () {
+	    _health = GetComponent<PlayerHealth>();
+	    _transform = gameObject.transform;
+	    _health.OnDeathAction += OnSpawn;
+        OnSpawn();
+	}
+
+    void OnSpawn() {
+        var spawnTrans = SpawnPoints.GetRandomSpawnPoint();
+        _transform.position = spawnTrans.position;
+        _transform.rotation = spawnTrans.rotation;
+        _health.ResetHealth();
+        if (OnSpawnAction != null) {
+            OnSpawnAction();
+        }
+    }
+
+    void OnDisable() {
+        OnSpawnAction = null;
+        if (_health.OnDeathAction != null) {
+            _health.OnDeathAction -= OnSpawn;
+        }
+    }
+}
---
+++ Assets/Scripts/Game Scripts/Gameplay/SpawnPoints/SpawnPoints.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPoints : MonoBehaviour {
+
+    private static List<GameObject> _spawnPoints = new List<GameObject>();
+
+    private static int _lastIndex;
+ 
+	// Use this for initialization
+	void Awake () {
+	    RandomMatchmaker.JoinedRoom += HandleOnJoinRoom;
+	}
+
+    void HandleOnJoinRoom(GameObject player) {
+        CollectSpawnPoints();
+    }
+
+    /// <summary>
+    /// Collects all spawn points in the level
+    /// </summary>
+    public void CollectSpawnPoints() {
+        //Grabs all spawn points that are a child of this game object
+        foreach (var child in GetComponentsInChildren<SpawnPoint>()) {
+            _spawnPoints.Add(child.gameObject);
+        }
+        Debug.Log(_spawnPoints.Count);
+        _lastIndex = _spawnPoints.Count;
+    }
+
+    /// <summary>
+    /// Returns a random spawn point to generate the player at
+    /// </summary>
+    /// <returns>A transform to spawn the player at</returns>
+    public static Transform GetRandomSpawnPoint() {
+        _lastIndex = _lastIndex + 1 >= _spawnPoints.Count ? 0 : _lastIndex + 1;  //If the index is higher then the list, it will reset back to 0
+        return _spawnPoints[_lastIndex].transform;
+    }
+}
