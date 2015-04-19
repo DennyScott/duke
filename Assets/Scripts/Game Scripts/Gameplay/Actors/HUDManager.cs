4239d60f9bd395e65d1b28d8f5cd6ee841d86bd3
Denny Scott <DennyScott301@gmail.com>
2015-04-19 01:34:50 -0500

    Added Event system to Player Health

---
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/HUDManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+	private PlayerHealth _playerHealth;
+
+	public Text HealthHud;
+	
+	// Use this for initialization
+	void Start () {
+		_playerHealth = GetComponent<PlayerHealth>();
+		InitalizeHealthHud();
+	}
+
+	void InitalizeHealthHud() {
+		_playerHealth.OnHealthLost += UpdateHealthHud;
+		_playerHealth.OnHealthGained += UpdateHealthHud;
+		_playerHealth.OnResetHealth += UpdateHealthHud;
+	}
+
+	void UpdateHealthHud(GameObject g) {
+		HealthHud.text = _playerHealth.PlayersHealth + "";
+	}
+}
---
+++ Assets/Scripts/Game Scripts/Gameplay/Actors/PlayerAnimations.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimations : MonoBehaviour {
+
+	private PlayerHealth playerHealth;
+
+	public Animator DamageAnimator;
+
+	// Use this for initialization
+	void Start () {
+		playerHealth = GetComponent<PlayerHealth>();
+		playerHealth.OnHealthLost += DamageAnimation;
+	}
+
+	void DamageAnimation(GameObject g) {
+		if (DamageAnimator != null) {
+			DamageAnimator.SetTrigger("Hit");
+		}
+	}
+}
